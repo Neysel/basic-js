f319ef1a0aa232ddf689363dc1bbfcf4bc18c671
Leyla <neyseltheblack@gmail.com>
2024-02-18 03:34:44 +0500

    another test

--- src/transform-array.js
+++ src/transform-array.js
@@ -17,33 +17,31 @@ function transform(arr) {
   if (!Array.isArray(arr) || arr === undefined) {
     throw Error("'arr' parameter must be an instance of the Array!");
   }
-
-  let newArr = arr;
-
-  const result = [];
-
-  for (let i = 0; i < newArr.length; i++) {
-    if (newArr[i] === "--discard-next" && i !== newArr.length - 1) {
-      i += 2;
-    } else if (newArr[i] === "--discard-prev" && i !== 0) {
-      result.pop();
-    } else if (newArr[i] === "--double-next" && i < newArr.length - 1) {
-      result.push(newArr[i + 1]);
-    } else if (newArr[i] === "--double-prev" && i !== 0) {
-      result.push(newArr[i - 1]);
-    } else {
-      result.push(newArr[i]);
+  let result = [];
+  for (let i = 0; i < arr.length; i++) {
+    switch (arr[i]) {
+      case "--discard-next":
+        i++;
+        break;
+      case "--discard-prev":
+        if (arr[i - 2] !== "--discard-next") {
+          result.pop();
+        }
+        break;
+      case "--double-next":
+        if (i < arr.length - 1) {
+          result.push(arr[i + 1]);
+        }
+        break;
+      case "--double-prev":
+        if (i > 1 && arr[i - 2] !== "--discard-next") {
+          result.push(arr[i - 1]);
+        }
+        break;
+      default:
+        result.push(arr[i]);
     }
   }
-
-  result = result.filter(
-    (elem) =>
-      elem !== "--discard-next" &&
-      elem !== "--discard-prev" &&
-      elem !== "--double-next" &&
-      elem !== "--double-prev"
-  );
-
   return result;
 }
 
