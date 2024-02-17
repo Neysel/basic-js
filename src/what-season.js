253bb1e09ce8d4faaee2825c7667d860f889826c
Leyla <neyseltheblack@gmail.com>
2024-02-18 03:17:56 +0500

    test

--- src/what-season.js
+++ src/what-season.js
@@ -11,7 +11,7 @@ const { NotImplementedError } = require("../extensions/index.js");
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  *
  */
-// function getSeason(date) {
+function getSeason(date) {
   if (date === undefined) {
     return "Unable to determine the time of year!";
   }
@@ -28,15 +28,16 @@ const { NotImplementedError } = require("../extensions/index.js");
   let givenDate = new Date(date);
   let month = givenDate.getUTCMonth();
 
-  if (month === 11 || month === 0 || month === 1) {
-    return "winter";
-  } else if (month === 2 || month === 3 || month === 4) {
-    return "spring";
-  } else if (month === 5 || month === 6 || month === 7) {
-    return "summer";
-  } else if (month === 8 || month === 9 || month === 10) {
-    return "autumn";
-  }
+  // if (month === 11 || month === 0 || month === 1) {
+  //   return "winter";
+  // } else if (month === 2 || month === 3 || month === 4) {
+  //   return "spring";
+  // } else if (month === 5 || month === 6 || month === 7) {
+  //   return "summer";
+  // } else if (month === 8 || month === 9 || month === 10) {
+  //   return "autumn";
+  // }
+  return "test";
 }
 
 module.exports = {
